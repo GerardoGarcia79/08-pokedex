bf7646ae488b35539dccdfd5a06d357959e974ab
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-04 17:59:58 -0600

    React native paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import '../gesture-handler';
 import React from 'react';
 import StackNavigator from './presentation/navigator/StackNavigator';
+import {PaperProvider} from 'react-native-paper';
 
 const App = () => {
-  return <StackNavigator />;
+  return (
+    <PaperProvider>
+      <StackNavigator />
+    </PaperProvider>
+  );
 };
 
 export default App;

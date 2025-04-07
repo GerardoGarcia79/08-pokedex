0f87b896bd49272b38288ae609e5482a302ea4f7
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-07 14:28:27 -0600

    Diseño del HomeScreen

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import {View} from 'react-native';
-import {ActivityIndicator, Button, Text} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
 import {getPokemons} from '../../../actions/pokemons';
 import {useQuery} from '@tanstack/react-query';
+import {PokeballBg} from '../../components/ui/PokeballBg';
 
 export const HomeScreen = () => {
-  const {isLoading} = useQuery({
+  const {isLoading, data = []} = useQuery({
     queryKey: ['pokemons'],
     queryFn: () => getPokemons(0),
     staleTime: 1000 * 60 * 60, // 60 min
@@ -13,15 +14,15 @@ export const HomeScreen = () => {
 
   return (
     <View>
-      <Text variant="displaySmall">HomeScreen</Text>
-
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <Button mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      )}
+      <PokeballBg style={styles.imgPosition} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgPosition: {
+    position: 'absolute',
+    top: -100,
+    right: -100,
+  },
+});

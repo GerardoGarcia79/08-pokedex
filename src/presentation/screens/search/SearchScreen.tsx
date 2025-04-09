8f8376cc9c60626c2deddd4007c28d6b99886c00
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-09 12:07:25 -0600

    SearchScreen - Diseño

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, View} from 'react-native';
+import {globalTheme} from '../../../config/theme/global-theme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {TextInput, useTheme} from 'react-native-paper';
+import {PokemonCard} from '../../components/pokemons/PokemonCard';
+import {Pokemon} from '../../../domain/entities/pokemon';
 
 export const SearchScreen = () => {
+  const theme = useTheme();
+  const {top} = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={[globalTheme.globalMargin, {paddingTop: top + 10}]}>
+      <TextInput
+        placeholder="Buscar Pokemon"
+        mode="flat"
+        autoFocus
+        autoCorrect={false}
+        onChangeText={value => console.log(value)}
+        value=""
+        style={{backgroundColor: theme.colors.background}}
+      />
+
+      {/* <ActivityIndicator style={{paddingTop: 20}} /> */}
+
+      <FlatList
+        data={[] as Pokemon[]}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{paddingTop: top + 20}}
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
